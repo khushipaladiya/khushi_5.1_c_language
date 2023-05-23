ad174f3dfaa83d71bd3823d90c95fc63c841bec1
khushi paladiya9 <sangitapaladiya9@gmail.com>
2023-05-23 10:39:03 +0530

    first commit

---
+++ KHUSHI1.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int A=22;
+int B=25;
+clrscr();
+if(A<B){
+printf("A is min");
+}else{
+printf("B is min");
+}
+getch();
+}
---
+++ KHUSHI2.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int A=-12;
+clrscr();
+if(A<0){
+printf("A is negative");
+} else{
+printf("B is positive");
+}
+getch();
+}
